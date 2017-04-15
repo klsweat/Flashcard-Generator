d681bcb40c114862662da68d052473f05a8551be
Kandace Sweat <kandacesweat@Kandaces-MacBook-Pro.local>
2017-04-15 07:42:13 -0400

    updated

--- newFlashcards.js
+++ newFlashcards.js
@@ -2,37 +2,37 @@
 //import the cloze and basiccard constructors
 var flashCard = require('./flashcardConstructors.js');
 
-console.log('     ');
+console.log('\n');
 console.log('BASIC CARDS');
-console.log('     ');
+console.log('\n');
 
 //create a new flashcard from BasicCard constructor 
 var firstPresident = new flashCard.BasicCard('Who was the first president of the United States?', 'George Washington');
 console.log("Front: " + firstPresident.front);
 console.log("Back: " + firstPresident.back); 
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 
 //create a new flashcard from BasicCard constructor 
 var presidentTerms = new flashCard.BasicCard('Who was the only President to serve more than two terms?', 'Franklin D. Roosevelt');
 console.log("Front: " + presidentTerms.front);
 console.log("Back: " + presidentTerms.back);
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 
 
 //create a new flashcard from BasicCard constructor 
 var oldPresident = new flashCard.BasicCard('Who was the oldest elected President?', 'Donald Trump');
 console.log("Front: " + oldPresident.front);
 console.log("Back: " + oldPresident.back);
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 console.log('CLOZE CARDS');
-console.log('     ');
+console.log('\n');
 
 //CLOZE DELETED FLASHCARDS
 
@@ -41,9 +41,9 @@ var firstPresident = new flashCard.ClozeCard('George Washington was the first pr
 console.log("Full Text: " + firstPresident.full);
 console.log("Cloze Text: " + firstPresident.cloze); 
 console.log("Partial Text: " + firstPresident.partial); 
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 
 
 //create a new flashcard from CLOZECARD constructor 
@@ -51,9 +51,9 @@ var presidentTerms = new flashCard.ClozeCard('Franklin D. Roosevelt was the only
 console.log("Full Text: " + presidentTerms.full);
 console.log("Cloze Text: " + presidentTerms.cloze); 
 console.log("Partial Text: " + presidentTerms.partial); 
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 
 
 
@@ -62,9 +62,9 @@ var oldPresident = new flashCard.ClozeCard('Donald Trump the oldest elected Pres
 console.log("Full Text: " + oldPresident.full);
 console.log("Cloze Text: " + oldPresident.cloze); 
 console.log("Partial Text: " + oldPresident.partial); 
-console.log('     ');
+console.log('\n');
 console.log('-----------------------------');
-console.log('     ');
+console.log('\n');
 
 
 
